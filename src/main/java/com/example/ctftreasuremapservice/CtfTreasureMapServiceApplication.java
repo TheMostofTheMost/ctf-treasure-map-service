945e07c829c8bac4835b90cec8541c6941adb768
Bogdan <bog.bgo@yandex.ru>
2023-07-25 19:23:27 +0300

    Наработки

--- src/main/java/com/example/ctftreasuremapservice/CtfTreasureMapServiceApplication.java
+++ src/main/java/com/example/ctftreasuremapservice/CtfTreasureMapServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CtfTreasureMapServiceApplication {
 
 	public static void main(String[] args) {
+//		https://www.dreamstime.com/hud-map-france-regions-cyberpunk-futuristic-digital-dark-blue-background-editable-stroke-vector-hud-map-france-image184244541
+		// hud map гугли
 		SpringApplication.run(CtfTreasureMapServiceApplication.class, args);
 	}
 
