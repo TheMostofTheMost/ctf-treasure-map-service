79efba29586024e5ad34e644f82e6189987496b1
Bogdan <bog.bgo@yandex.ru>
2023-07-25 19:09:41 +0300

    Пустой каркас

---
+++ src/main/java/com/example/ctftreasuremapservice/CtfTreasureMapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ctftreasuremapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtfTreasureMapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtfTreasureMapServiceApplication.class, args);
+	}
+
+}
