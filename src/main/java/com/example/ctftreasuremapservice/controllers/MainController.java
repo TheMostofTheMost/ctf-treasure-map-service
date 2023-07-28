86400a9c6bca87c5a0f6a5f7baf433815e206ff3
Bogdan <bog.bgo@yandex.ru>
2023-07-29 00:51:43 +0300

    Картинки на гексы

--- src/main/java/com/example/ctftreasuremapservice/controllers/MainController.java
+++ src/main/java/com/example/ctftreasuremapservice/controllers/MainController.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public ModelAndView getMainPage() {
-        ModelAndView modelAndView = new ModelAndView("main-page.html");
+        ModelAndView modelAndView = new ModelAndView("main-page-non-authorized.html");
+        List<Integer> test = List.of(7, 4, 3, 8, 5);
+        modelAndView.addObject("containers", test);
         return modelAndView;
     }
 
