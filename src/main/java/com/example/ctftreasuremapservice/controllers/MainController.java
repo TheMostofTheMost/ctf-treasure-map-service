2aa273b7cdcaa35070cbfae073ac3fbaf8db6c90
Bogdan <bog.bgo@yandex.ru>
2023-08-13 20:55:55 +0300

    Границы гексов

--- src/main/java/com/example/ctftreasuremapservice/controllers/MainController.java
+++ src/main/java/com/example/ctftreasuremapservice/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.example.ctftreasuremapservice.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -12,8 +13,8 @@ public class MainController {
 
     @GetMapping("/")
     public ModelAndView getMainPage() {
-//        ModelAndView modelAndView = new ModelAndView("main-page-non-authorized.html");
-        ModelAndView modelAndView = new ModelAndView("main-page-authorized.html");
+        ModelAndView modelAndView = new ModelAndView("main-page-non-authorized.html");
+//        ModelAndView modelAndView = new ModelAndView("main-page-authorized.html");
         List<Integer> test = List.of(7, 4, 3, 8, 5);
         modelAndView.addObject("containers", test);
         return modelAndView;
